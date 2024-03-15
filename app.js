19abc5d5b46063e1ae02fb5eb7f1cc42f317e2db
TuriniMattia <turinimattia96@gmail.com>
2024-03-15 03:17:56 +0100

    [add]difficult

--- app.js
+++ app.js
@@ -3,23 +3,48 @@ const pressBtn = document.getElementById(playGame)
 
 const gridEl = document.querySelector(".grid")
 console.log(gridEl)
+const levelSelectEl = document.getElementById("level")
+// let valueEasy = document.getElementById("easy").value;
+// let valueMedium = document.getElementById("medium").value;
+// let valueHard = document.getElementById("hard").value;
 
-const size = 10
-const numOfGridIt = size * size 
+playGame.addEventListener('click', readyP1)
 
-for (let i = 0; i < numOfGridIt; i++){
-    const num = i + 1
-    console.log(num)
+function readyP1() {
+    
+    const size = getSize()
+    const numOfGridIt = size ** 2
+    gridEl.innerHTML = ""
 
-    const gridItem = document.createElement('div');
-    gridItem.className = 'gridEl';
-    gridItem.innerHTML = num ;
+    for (let i = 0; i < numOfGridIt; i++) {
+        const num = i + 1
+        console.log(num)
 
-    gridEl.append(gridItem);
 
-    gridItem.addEventListener('click', function(){
-        console.log('click sulla casella', num)
-        gridItem.classList.toggle('bg-blue')
+        const gridItem = document.createElement('div');
+        gridItem.className = 'gridEl';
+        gridItem.innerHTML = num;
+        gridItem.style.width = `repeat(${size}, 1fr)`
+
+        gridEl.append(gridItem);
+
+        gridItem.addEventListener('click', function () {
+            console.log('click sulla casella', num)
+            gridItem.classList.toggle('bg-blue')
+        }
+        )
     }
-    )
+}
+
+function getSize (){
+    let size = 7
+    let level = levelSelectEl.value
+
+    if(level === "easy"){
+        size = 10
+    } else if(level === "medium"){
+        size = 9
+    }
+
+    return size 
 }
